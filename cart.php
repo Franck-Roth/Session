154e847a6c80e9a6d53d9362d956cf989582611a
Jeffo7980 <61733249+Jeffo7980@users.noreply.github.com>
2020-04-27 15:14:27 +0200

    Add files via upload

--- cart.php
+++ cart.php
@@ -1,7 +1,41 @@
-<?php require 'inc/head.php'; ?>
-<section class="cookies container-fluid">
-    <div class="row">
-        TODO : Display shopping cart items from $_COOKIES here.
-    </div>
-</section>
+<?php
+session_start();
+require 'inc/head.php';
+require 'inc/data/products.php';
+$quantities = array_count_values($_SESSION['cart']);?>
+    <section class="cookies container-fluid">
+        <div class="row">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Ref.</th>
+                    <th>Produit</th>
+                    <th>Prix U.</th>
+                    <th>Quantité</th>
+                    <th>Prix Tot.</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php $totalPrice = 0; ?>
+                <?php foreach ($quantities as $productId => $quantities) { ?>
+                    <tr>
+                        <td><?= $productId; ?></td>
+                        <td><?= $catalog[$productId]['name']; ?></td>
+                        <td><?= $quantities; ?></td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+                <tfoot>
+                <tr>
+                    <td colspan="4">
+                        Total :
+                    </td>
+                    <td>
+                        <?= $totalPrice; ?>€
+                    </td>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
+    </section>
 <?php require 'inc/foot.php'; ?>
